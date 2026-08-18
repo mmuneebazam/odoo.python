7ba53f976d334267a12b03a0a5bb8b22f6a5c122
Muneeb Azam <azammuneeb5@gmail.com>
2026-08-18 12:09:04 +0500

    Add condition to check final price against 1000

--- exercise1.py
+++ exercise1.py
@@ -5,3 +5,7 @@ discount_percent = 10
 total_before_discount = price * quantity
 discount_amount = total_before_discount * (discount_percent / 100)
 final_price = total_before_discount - discount_amount
+if final_price > 1000:
+    print("Final price is above 1000")
+else:
+    print("Final price is 1000 or below")
