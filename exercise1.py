f484f48d3c199e452c9c5502ba7dc430e1841162
Muneeb Azam <azammuneeb5@gmail.com>
2026-08-18 12:02:56 +0500

    Add pricing calculation with discount

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+price = 1500.0
+quantity = 3
+discount_percent = 10
+
+total_before_discount = price * quantity
+discount_amount = total_before_discount * (discount_percent / 100)
+final_price = total_before_discount - discount_amount
